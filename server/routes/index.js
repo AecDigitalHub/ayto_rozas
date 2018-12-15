55b6ed258d68247c8192fb97838a631824c2e3ac
AecDigitalHub <aecgithub@gmail.com>
2018-12-15 10:52:21 +0100

    commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,13 +3,13 @@ const router  = express.Router();
 const apiRoutes = require('./api/index.controller');
 
 
-// serve angular front end files from root path
-router.use('/', express.static('app', { redirect: false }));
+// // serve angular front end files from root path
+// router.use('/', express.static('app', { redirect: false }));
  
-// rewrite virtual urls to angular app to enable refreshing of internal pages
-router.get('*', function (req, res, next) {
-    res.sendFile(path.resolve('app/index.html'));
-});
+// // rewrite virtual urls to angular app to enable refreshing of internal pages
+// router.get('*', function (req, res, next) {
+//     res.sendFile(path.resolve('app/index.html'));
+// });
 
 router.use('/api', apiRoutes);
 
